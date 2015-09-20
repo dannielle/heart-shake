3ed000f322f9efe35380384609b0e1cb8c2c1cdc
dannielle <dannielle.del@gmail.com>
2015-09-20 17:32:51 -0400

    detects punch from arduino, logic for shaking

---
+++ Assets/InputScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class InputScript : MonoBehaviour {
+
+	SerialPort stream = new SerialPort("/dev/cu.usbserial-DN00BXVG", 9600);
+	int PUNCH_INT = 1;
+	int SHAKE_INT = 2;
+
+	int punchCount;
+
+	// Use this for initialization
+	void Start () {
+		punchCount = 0;
+		stream.Open ();
+		stream.ReadTimeout = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int inp = 0;
+
+		if (stream.IsOpen) {
+			try {
+				inp = stream.ReadByte();
+				//print (inp);
+			} catch (System.Exception) {
+
+			}
+		}
+			
+		bool isPunched = checkForPunch (inp);
+		if (isPunched) {
+			punchCount ++;
+			punch();
+		}
+
+		bool isShaken = checkForShaken (punchCount);
+		if (isShaken) {
+			punchCount = 0;
+			shake();
+		}
+	}
+
+	bool checkForPunch(int input){
+		if (input == PUNCH_INT) {
+			return true;
+		}
+		return false;
+	}
+
+	bool checkForShaken(int punchCount) {
+		if (punchCount > 2) {
+			return true;
+		}
+		return false;
+	}
+
+	void punch() {
+		print ("PUNCH");
+	}
+	
+	void shake() {
+		print ("SHAKE");
+	}
+
+}
